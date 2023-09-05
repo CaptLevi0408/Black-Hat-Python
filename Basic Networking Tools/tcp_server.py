8dfb2d73bbc198fa3ab9d66f78da7e8817cbe13d
CaptLevi0824 <119733356+CaptLevi0408@users.noreply.github.com>
2023-09-05 11:27:00 -0700

    Update tcp_server.py

--- Basic Networking Tools/tcp_server.py
+++ Basic Networking Tools/tcp_server.py
@@ -1,9 +1,14 @@
 import socket
 import threading
+import signal
+import sys
 
 # Define the IP address and port to listen on
 IP = '0.0.0.0'  # Listen on all available network interfaces
 PORT = 1234     # Port number to listen on
+
+# Global variable to track whether the server should continue running
+server_running = True
 
 # Function to handle incoming client connections
 def handle_client(client_socket):
@@ -15,8 +20,18 @@ def handle_client(client_socket):
         # Send a simple acknowledgment back to the client
         sock.send(b'ACK')
 
+# Signal handler for ctrl+c
+def signal_handler(sig, frame):
+    global server_running
+    print("[*] Exiting server...")
+    server_running = False
+    sys.exit(0)
+
 # Main function to set up the server
 def main():
+    # Register the signal handler for ctrl+c
+    signal.signal(signal.SIGINT, signal_handler)
+    
     # Create a socket object using IPv4 and TCP
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -27,15 +42,21 @@ def main():
     server.listen(5)
     print(f'[*] Listening on {IP}:{PORT}')
 
-    while True:
-        # Accept an incoming connection, client is a new socket object
-        # and address is the client's address (IP and port)
-        client, address = server.accept()
-        print(f'[*] Accepted connection from {address[0]}:{address[1]}')
+    while server_running:
+        try:
+            # Accept an incoming connection, client is a new socket object
+            # and address is the client's address (IP and port)
+            client, address = server.accept()
+            print(f'[*] Accepted connection from {address[0]}:{address[1]}')
 
-        # Create a new thread to handle the client's communication
-        client_handler = threading.Thread(target=handle_client, args=(client,))
-        client_handler.start()
+            # Create a new thread to handle the client's communication
+            client_handler = threading.Thread(target=handle_client, args=(client,))
+            client_handler.start()
+        except KeyboardInterrupt:
+            # If ctrl+c is pressed during server operation, terminate gracefully
+            print("[*] Server interrupted by user.")
+            server.close()
+            sys.exit(0)
 
 if __name__ == '__main__':
     main()
